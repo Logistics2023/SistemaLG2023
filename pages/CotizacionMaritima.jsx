d95c09d3f8af494591525ff99b8a646b3bb9bc40
Logistics2023 <131830463+Logistics2023@users.noreply.github.com>
2023-05-17 00:13:12 +0200

    Update CotizacionMaritima.jsx
    DC

--- pages/CotizacionMaritima.jsx
+++ pages/CotizacionMaritima.jsx
@@ -128,13 +128,15 @@ function CotizacionMaritima() {
         }
         setCalc({ ...calc, ...object })
     }
-
+    function round(num) {
+        var m = Number((Math.abs(num) * 100).toPrecision(15));
+        return Math.round(m) / 100 * Math.sign(num);
+    }
     function reducer(e, index, counter, prod, total) {
         let product = e.target.value * calc[`${counter}${index}`]
-
         let data = {
             ...calc,
-            [e.target.name]: e.target.value,
+            [e.target.name]:  e.target.value,
             [`${prod}${index}`]: product,
         }
 
@@ -149,15 +151,15 @@ function CotizacionMaritima() {
                 return ac
             }
             let res = str.includes(prod)
-            let r = res ? i[1] + ac : ac
+            let r = res ? i[1] * 1 + ac * 1 : ac * 1
             return r
         }, 0)
 
         let object = {
             [e.target.name]: e.target.value,
-            [`${prod}${index}`]: product,
-            PRODUCTOFLETETOTAL: prod === 'PRODUCTFLETE' ? red : data['PRODUCTOFLETETOTAL'],
-            PRODUCTOTOTAL: prod === 'PRODUCT' ? red : data['PRODUCTOTOTAL'],
+            [`${prod}${index}`]: round(product).toFixed(2) ,
+            PRODUCTOFLETETOTAL: prod === 'PRODUCTFLETE' ? round(red).toFixed(2)  : data['PRODUCTOFLETETOTAL'],
+            PRODUCTOTOTAL: prod === 'PRODUCT' ? round(red).toFixed(2)  : data['PRODUCTOTOTAL'],
         }
         return object
     }
@@ -548,7 +550,7 @@ function CotizacionMaritima() {
                     <br />
 
                     <div className={style.inputsSemi}>
-                        <label htmlFor="">Costo Total</label><input type="text" defaultValue={calc.PRODUCTOTOTAL && calc.PRODUCTOFLETETOTAL ? calc.PRODUCTOTOTAL + calc.PRODUCTOFLETETOTAL : (calc.PRODUCTOTOTAL ? calc.PRODUCTOTOTAL : (calc.PRODUCTOFLETETOTAL && calc.PRODUCTOFLETETOTAL))} />
+                        <label htmlFor="">Costo Total</label><input type="text" defaultValue={(calc.PRODUCTOTOTAL && calc.PRODUCTOFLETETOTAL ? (calc.PRODUCTOTOTAL *1 + calc.PRODUCTOFLETETOTAL *1).toFixed(2): (calc.PRODUCTOTOTAL ? calc.PRODUCTOTOTAL : (calc.PRODUCTOFLETETOTAL && calc.PRODUCTOFLETETOTAL)))} />
                     </div>
 
                     <br />
