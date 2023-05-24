1bce6a6faafca8a9205227e9193e9d6e938fff6b
Logistics2023 <131830463+Logistics2023@users.noreply.github.com>
2023-05-24 14:09:05 -0400

    Update CotizacionMaritima.jsx
    Cc

--- pages/CotizacionMaritima.jsx
+++ pages/CotizacionMaritima.jsx
@@ -127,6 +127,7 @@ function CotizacionMaritima() {
             [e.target.name]: e.target.name.includes('UNITARIO') ? formatoMexico(round(e.target.value).toFixed(2)) : e.target.value,
         setCalc({ ...calc, ...object })
     }
+    }
     function round(num) {
         var m = Number((Math.abs(num) * 100).toPrecision(15));
         return Math.round(m) / 100 * Math.sign(num);
